dcf2e5f84f2864a8e8532c52ffe48b78fe68bab5
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-09-07 16:57:01 -0400

    Add optimal solution for LC138

--- linked_lists/138-copy-list-with-random-pointer.py
+++ linked_lists/138-copy-list-with-random-pointer.py
@@ -1,8 +1,8 @@
-
 from typing import Optional
 
+# Definition for a Node.
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    def __init__(self, x: int, next: Optional['Node'] = None, random: Optional['Node'] = None):
         self.val = int(x)
         self.next = next
         self.random = random
@@ -13,15 +13,73 @@ class Solution:
         Using dictionary to store each node's copy
 
         Time: O(n)
-        Space: O(n), storing each node's copy in a dictionary
+        Space: O(1) excluding space for copy list
         """
         if head is None:
             return head
 
-        # maps original nodes to their copies
+        # First copy the main list first
+        cur = head
+
+        # interleave each node with its copy
+        # Old List: A --> B --> C
+        # InterWeaved List: A --> A' --> B --> B' --> C --> C'
+        while cur is not None:
+            nxt = cur.next
+
+            # interweave
+            cur_prime = Node(cur.val)
+            cur.next = cur_prime
+            cur_prime.next = nxt
+
+            cur = cur.next.next
+        
+        # remember to set cur back to head
+        cur = head
+        res = Node(-999999)
+        res_cur = res
+
+        while cur is not None:
+            # copy the random pointer to cur_prime
+            cur_prime = cur.next
+            if cur.random is not None:
+                cur_prime.random = cur.random.next
+
+            # set cur to next original node
+            cur = cur.next.next
+
+            # move the copy node to result list
+            cur_prime.next = None
+            res_cur.next = cur_prime
+            res_cur = res_cur.next
+
+        return res.next
+
+        """
+        Note: this algorithm mutates the original list
+        To properly maintain the original list, we need 3 while loops:
+        first for interleaving
+        second for copying random pointer
+        third for separating the two lists
+
+        Random pointers can point backwards so it is not
+        safe to separate the two lists in the second loop
+        """
+
+    def copyRandomList2(self, head: Optional[Node]) -> Optional[Node]:
+        """
+        Using dictionary to store each node's copy
+
+        Time: O(n)
+        Space: O(n) excluding space for copy list
+        """
+        if head is None:
+            return head
+
+        # maps original node to its copy
         copy: dict[Node, Node] = {}
 
-        # First copy the list first
+        # First copy the main list first
         cur = head
 
         cpy_res =  Node(-9999)
